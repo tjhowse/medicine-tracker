6b607b868d0a09981879cc957af3f673347426e4
Travis Howse <tjhowse@gmail.com>
2025-02-19 13:16:43 +1000

    Slightly less ugly.

--- web/med_script.js
+++ web/med_script.js
@@ -2,7 +2,7 @@
 const medicineLog = document.getElementById('medicine-log-table');
 const medicineLogTable = document.createElement('table');
 const medicineLogHeaderRow = document.createElement('tr');
-medicineLogHeaderRow.innerHTML = '<th>Time</th><th>Count</th><th>Medicine</th><th>Note</th>';
+medicineLogHeaderRow.innerHTML = '<th>Time</th><th>Count</th><th>Medicine</th><th>Note</th><th>Delete</th>';
 medicineLogTable.appendChild(medicineLogHeaderRow);
 medicineLog.appendChild(medicineLogTable);
 populateMedicineLogTable();
